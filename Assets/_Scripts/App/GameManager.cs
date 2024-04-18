ae1fdcc88438f8cf05fd7e1842591f96c2d9d9c5
GBB1123456 <1453062510@qq.com>
2024-04-18 21:52:53 +0800

    excel表基本配完;

--- Assets/_Scripts/App/GameManager.cs
+++ Assets/_Scripts/App/GameManager.cs
@@ -6,19 +6,49 @@ using ZXKFramework;
 public class GameManager : MonoSingleton<GameManager>
 {
     GameModel gameModel;
+
+    int id;
+    bool isa = false;
     // Start is called before the first frame update
     void Start()
     {
         gameModel = MVC.GetModel<GameModel>();
+        id = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isa = true;
+        }
+
+        if (isa)
+        {
+            id++;
+            MainData main = gameModel.excel.GetMainDataid(id);
+            Sprite sp = Resources.Load<Sprite>(main.SpritePath);
+            if (string.IsNullOrEmpty(main.TopTxt) || string.IsNullOrEmpty(main.Txt) || sp == null)
+            {
+                Debug.Log("上面三个判断有空，无法赋值，检查表中名字为：" + id);
+            }
+            else
+            {
+                Game.Instance.uiManager.ShowUI<JieShaoWnd>(null, main.TopTxt, sp, main.Txt);
+            }
+
+            isa = false;
+        }
+
     }
 
 
+    /// <summary>
+    /// 显示介绍界面
+    /// </summary>
+    /// <param name="go"></param>
     public void ShowJieShaoWnd(GameObject go)
     {
         MainData main = gameModel.excel.GetMainDataName(go.name);
